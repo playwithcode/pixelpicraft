fd34a9d4a74dac1bab6d3e2ac60de024f880c4ca
Samuel Freeman <soundmaking@merfasmean.com>
2016-06-03 00:21:41 +0100

    edit make_world_flat

--- make_world_flat.py
+++ make_world_flat.py
@@ -5,18 +5,20 @@ import time
 
 w = World(server.address)
 
-w.say('Making the world flat...')
-w.say('please wait - this will take a while!')
-time.sleep(3) # give time for the chat to display
-
 v1 = Vector(-128, 0, -128)
 v2 = Vector(128, 64, 128)
 v3 = Vector(-128, -64, -128)
-v4 = Vector(128, 0, 128)
+v4 = Vector(128, -1, 128)
+
+w.say('Making the world flat')
+w.say('...please wait ')
+w.say('. . . . . . . . . . . . this will take a long time!')
+time.sleep(3)  # give time for the chat to display
 
 w.blocks[v1:v2 + 1] = Block('air')
 w.blocks[v3:v4 + 1] = Block('sandstone')
 
-time.sleep(3)
-w.say('flat world')
+w.say('Thanks for waiting - flat world complete!')
+
+w.player.pos = (X+Y+Z)
 
